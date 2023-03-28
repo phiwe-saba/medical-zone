a0371f711335e5cc877d36601b2866a8ee97675e
phiwe saba <sabasbulele81@gmail.com>
2023-03-28 21:11:20 +0200

    added JavaScript to the appointment form.

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('appointment-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const dateInput = document.getElementById('date');
+const timeInput = document.getElementById('time');
+const reasonInput = document.getElementById('reason');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Validate form data
+  if (nameInput.value === '' || emailInput.value === '' || phoneInput.value === '' || dateInput.value === '' || timeInput.value === '' || reasonInput.value === '') {
+    alert('Please fill in all fields.');
+    return false;
+}
+
+// Submit form data
+const formData = new FormData(form);
+const xhr = new XMLHttpRequest();
+xhr.open('POST', 'your-server-url-here');
+xhr.send(formData);
+
+alert
+})
